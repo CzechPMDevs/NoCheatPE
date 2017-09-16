5956411da5d298cd1d74a95165ceaaa72bea8533
kaliiks <kaliik1@seznam.cz>
2017-09-16 16:10:35 +0300

    Update NoCheatPlayer.php

--- NoCheatPE/src/NoCheatPE/NoCheatPlayer.php
+++ NoCheatPE/src/NoCheatPE/NoCheatPlayer.php
@@ -7,6 +7,17 @@ use pocketmine\Player;
 class NoCheatPlayer extends Player{
   
   public function __construct(SourceInterface $interface, $clientID, $ip, $port){
+        
+			$cheats = NoCheatPE::getInstance()->getCheats();
+		
+			foreach($cheats as $cheatClass) {
+				/** @var DetectionHook $cheat */
+				$cheat = new $cheatClass($this);
+				$this->cheats[] = $cheat;
+			}
+		}
+
+    
         parent::__construct($interface, $clientID, $ip, $port);
         
   }
