3fa4700d5bf02d7a26fc3b9624de30600ac7f3a0
kaliiks <kaliik1@seznam.cz>
2017-09-16 16:01:21 +0300

    Update NoCheatPlayer.php

--- NoCheatPE/src/NoCheatPE/NoCheatPlayer.php
+++ NoCheatPE/src/NoCheatPE/NoCheatPlayer.php
@@ -5,4 +5,10 @@ namespace NoCheatPE\cheats;
 use pocketmine\Player;
 
 class NoCheatPlayer extends Player{
+  
+  public function __construct(SourceInterface $interface, $clientID, $ip, $port){
+        parent::__construct($interface, $clientID, $ip, $port);
+        
+  }
+  
 }
