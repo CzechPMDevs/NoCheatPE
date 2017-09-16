52e13b17a312b7976fa4dc1b20e9c780c570c293
kaliiks <kaliik1@seznam.cz>
2017-09-16 16:31:37 +0300

    Update NoCheatPlayer.php

--- NoCheatPE/src/NoCheatPE/NoCheatPlayer.php
+++ NoCheatPE/src/NoCheatPE/NoCheatPlayer.php
@@ -7,18 +7,21 @@ class NoCheatPlayer extends Player{
   public $cheats = [];
 	
   public function __construct(SourceInterface $interface, $clientID, $ip, $port){
-        
 			$cheats = NoCheatPE::getInstance()->getCheats();
-		
+			if(count($cheats > 0){
 			foreach($cheats as $cheatClass) {
 				/** @var CheatDetection $cheat */
 				$cheat = new $cheatClass($this);
 				$this->cheats[] = $cheat;
-			
 		}
-    
-        parent::__construct($interface, $clientID, $ip, $port);
-        
+		}
+        parent::__construct($interface, $clientID, $ip, $port);      
+  }
+  
+  public function onTick($ticks){
+         foreach($this->cheats as $cheat){
+             //Tick
+         }
   }
   
 }
