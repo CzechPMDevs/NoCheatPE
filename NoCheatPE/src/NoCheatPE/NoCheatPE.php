062d365394daaca09798e6a02310aa2a812a300e
kaliiks <kaliik1@seznam.cz>
2017-09-16 15:01:11 +0300

    Update NoCheatPE.php

--- NoCheatPE/src/NoCheatPE/NoCheatPE.php
+++ NoCheatPE/src/NoCheatPE/NoCheatPE.php
@@ -7,4 +7,18 @@ class NoCheatPE extends PluginBase{
   
   /** @var NoCheatPE $instance */
   public static $instance;
+  
+  public $movement_cheats = [];
+  
+  public function onEnable(){
+    self::$instance = $this;
+    PluginBase::onEnable();
+  }
+  
+  public static function getInstance(){
+    if(!is_null(self::$instance)){
+      return self::$instance;
+     }
+    return null;
+  }
 }
