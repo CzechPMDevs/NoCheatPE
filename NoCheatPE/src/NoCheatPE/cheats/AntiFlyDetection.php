ad8f721b607264052588e8d7a2b05cb3f92e9bba
kaliiks <kaliik1@seznam.cz>
2017-09-16 16:57:53 +0300

    Update AntiFlyDetection.php

--- NoCheatPE/src/NoCheatPE/cheats/AntiFlyDetection.php
+++ NoCheatPE/src/NoCheatPE/cheats/AntiFlyDetection.php
@@ -1,1 +1,14 @@
+<?php
 
+namespace NoCheatPE\cheats;
+
+use pocketmine\math\Position;
+
+class AntiFlyDetection extends CheatDetection{
+  
+  public function onTick($currentTick){
+    
+  }
+  
+  
+}
