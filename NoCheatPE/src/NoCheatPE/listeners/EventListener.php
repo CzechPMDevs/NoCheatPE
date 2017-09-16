143c7b0c98df95f322168ae6cd3128ead356af99
kaliiks <kaliik1@seznam.cz>
2017-09-16 15:16:03 +0300

    Update EventListener.php

--- NoCheatPE/src/NoCheatPE/listeners/EventListener.php
+++ NoCheatPE/src/NoCheatPE/listeners/EventListener.php
@@ -1,1 +1,20 @@
+?<php
 
+namespace NoCheatPE\listeners;
+      
+use pocketmine\event\Listener;
+use NoCheatPE\NoCheatPlayer;
+      
+class EventListener implements Listener{
+      
+      /** @var NoCheatPE $plugin */
+      public $plugin;
+
+      public function __construct(NoCheatPE $plugin){
+           $this->plugin = $plugin;
+      }
+  
+      public function onPlayerCreation(PlayerCreationEvent $e){
+          $e->setPlayerClass(NoCheatPlayer::class);
+      }
+}
