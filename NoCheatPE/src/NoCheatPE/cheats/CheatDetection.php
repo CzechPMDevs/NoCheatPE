e92c4737b7cbabaf90ab8d8df1a8ce46e4e6c3b0
kaliiks <kaliik1@seznam.cz>
2017-09-16 15:31:24 +0300

    Update CheatDetection.php

--- NoCheatPE/src/NoCheatPE/cheats/CheatDetection.php
+++ NoCheatPE/src/NoCheatPE/cheats/CheatDetection.php
@@ -1,1 +1,33 @@
+<?php
 
+namespace NoCheatPE\cheats;
+
+abstract class CheatDetection{
+  
+  
+  /** @var NoCheatPlayer $player */
+  public $player;
+  
+  public function __construct(NoCheatPlayer $player){
+    $this->player = $player;
+  }
+  
+  public function onCheatDetect($cheatType){
+    //Call event to make devs able to use it in another plugins TODO
+    //add point TODO
+    //Check player status about points TODO
+  }
+  
+  
+  public function getPlayer(){
+    return $this->player;
+  }
+  
+  public function onTick(){ //Will be used in player class tick function
+    
+  }
+  
+  
+  
+  
+}
